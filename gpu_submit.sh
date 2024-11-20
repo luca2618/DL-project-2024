50d5db1715133df2d7a9ea0352de338876aba5fd
Lucas Sylvester <lucassylvester02@gmail.com>
2024-11-20 14:44:07 +0100

    Update gpu_submit.sh

--- gpu_submit.sh
+++ gpu_submit.sh
@@ -5,11 +5,11 @@
 ### -- set the job Name --
 #BSUB -J GPU_train
 ### -- ask for number of cores (default: 1) --
-#BSUB -n 2
+#BSUB -n 4
 ### -- Select the resources: 1 gpu in exclusive process mode --
 #BSUB -gpu "num=1:mode=exclusive_process"
 ### -- set walltime limit: hh:mm --  maximum 24 hours for GPU-queues right now
-#BSUB -W 24:00
+#BSUB -W 1:00
 # request 16GB of system-memory
 #BSUB -R "rusage[mem=16GB]"
 ### -- send notification at start --
