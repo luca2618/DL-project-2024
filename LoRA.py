3fa4375068fcab378f0a9ceb9c2623293c321ca6
Lucas Sylvester <lucassylvester02@gmail.com>
2024-12-02 12:59:03 +0100

    Update LoRA.py

--- LoRA.py
+++ LoRA.py
@@ -49,7 +49,7 @@ model = get_peft_model(model, lora_config)
 
 # 3. Prepare the dataset
 if dataset_name == "Bio":
-    ds = load_dataset(datasets[dataset_name], "pqa_labeled", cache_dir='./cache')
+    ds = load_dataset(datasets[dataset_name], "pqa_artificial", cache_dir='./cache')
 else:
     ds = load_dataset(datasets[dataset_name], cache_dir='./cache')
 
