1af490029a7953a5bf4595319bb38132150ecae6
lucas <>
2024-12-04 17:04:51 +0100

    Change batch size, change cummulative gradient so it runs faster

--- LoRA.py
+++ LoRA.py
@@ -16,10 +16,10 @@ models = { # name : path (model, dataset)
     "MetaMath-Mistral-7B" : "meta-math/MetaMath-Mistral-7B"
 }
 datasets = {
-    "Math" : "meta-math/MetaMathQA",
+    "Math" : "meta-math/MetaMathQA-40k",
     "Bio" : "qiaojin/PubMedQA", # Dataset doesn't exist (Find another one)
 }
-labels = {"Math" : ["query", "reponse"],
+labels = {"Math" : ["query", "response"],
           "Bio" : ["question", "long_answer"]
           }
 
@@ -39,8 +39,8 @@ print("selected:")
 print("model:"+model_name)
 print("dataset:"+dataset_name)
 
-tokenizer = AutoTokenizer.from_pretrained(models[model_name], use_auth_token="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", cache_dir='./cache', device_map=torch_device)
-model = AutoModelForCausalLM.from_pretrained(models[model_name], use_auth_token="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", cache_dir='./cache', torch_dtype=torch.float16, device_map=torch_device)
+tokenizer = AutoTokenizer.from_pretrained(models[model_name], use_auth_token="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", cache_dir='./cache')
+model = AutoModelForCausalLM.from_pretrained(models[model_name], use_auth_token="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", cache_dir='./cache', torch_dtype=torch.float16)
 tokenizer.pad_token = tokenizer.eos_token
 
 # 2. Define the LoRA configuration
@@ -55,19 +55,24 @@ lora_config = LoraConfig(
 # Wrap the model with LoRA
 model = get_peft_model(model, lora_config)
 
+
 # 3. Prepare the dataset
 if dataset_name == "Bio":
     ds = load_dataset(datasets[dataset_name], "pqa_artificial", cache_dir='./cache')
 else:
     ds = load_dataset(datasets[dataset_name], cache_dir='./cache')
 
-ds = ds['train'].to_pandas()[:120000]
+
+total_data_points = 40000
+
+ds = ds['train'].to_pandas()[:total_data_points]
 
 # Split the dataset
-split = 0.9
+split = 0.95
 split_idx = int(len(ds) * split)
 train_data_raw = ds[:split_idx]
 eval_data_raw = ds[split_idx:]
+
 
 # Preprocess the dataset
 train_data = []
@@ -94,23 +99,45 @@ def preprocess_function(example):
 train_dataset = Dataset.from_list(train_data).map(preprocess_function, batched=True)
 eval_dataset = Dataset.from_list(eval_data).map(preprocess_function, batched=True)
 
+
+train_data_length = len(train_dataset)
+epochs = 1.5
+batch_size = 4
+gradient_accumulation_steps = 1
+effective_batch_size = gradient_accumulation_steps*batch_size
+
+total_steps = epochs*train_data_length/effective_batch_size
+
+
+
 # 4. Training configuration
 training_args = TrainingArguments(
     output_dir=f"lora_model-{model_name}_dataset-{dataset_name}_v1",
-    evaluation_strategy="steps", # Evaluate every 500 steps
+    eval_strategy="steps", # Evaluate every 500 steps
     save_strategy="steps", # Save every 500 steps
-    save_steps=500,
-    per_device_train_batch_size=2, # Batch size per GPU
-    gradient_accumulation_steps=4, # Accumulate gradients
-    num_train_epochs=1.5,
+    save_steps=int(total_steps/10),
+    use_cpu = False,
+    per_device_train_batch_size=batch_size, # Batch size per GPU
+    #auto_find_batch_size = True,
+    gradient_accumulation_steps=gradient_accumulation_steps, # Accumulate gradients
+    num_train_epochs=epochs,
     learning_rate=2e-4,
     fp16=True, # Use mixed precision
     logging_strategy="steps", # Log every 100 steps
     logging_dir="./logs", # Logs
-    logging_steps=100, # Log every 100 steps
-    save_total_limit=2, # Save only the last 2 checkpoints
+    logging_steps=int(total_steps/15), # Log every 100 steps
+    save_total_limit=10, # Save only the last 10 checkpoints
     report_to="none", # Don't report to Hugging Face
 )
+
+
+#training_args = training_args.set_dataloader(train_batch_size=10, eval_batch_size=64)
+
+print("#########################################################################")
+print(f"Total steps:{total_steps}")
+print(f"Batch size: {training_args.train_batch_size}")
+print(f"Effective Batch size: {effective_batch_size}")
+print("#########################################################################")
 
 # 5. Trainer setup
 trainer = Trainer(
